f62c34fe30e2e83b8c28bae1b982ede5377f635d
TweetyTheBird <42115728+TweetyTheBird@users.noreply.github.com>
2018-11-28 20:04:22 +0200

    homework1

---
+++ homework1/homework1.php
@@ -0,0 +1,50 @@
+<?php
+
+$name = 'john';
+var_dump($name);
+echo '<br />';
+
+$message = "$name said learning PHP is fun";
+echo $message;
+echo '<br />';
+
+$word1 = "Hello";
+$word2 = "PHP";
+
+echo "$word1 $word2";
+echo '<br />';
+
+$bank_balance = 9999; 
+echo "Your bank balance is $bank_balance bgn.";
+echo '<br />';
+
+$cars = array('Lada', 'Honda', 'BMW', 'Ford');
+print_r($cars);
+echo '<br />';
+
+//chetno / nechetno
+$num = 6;
+
+$result = ($num % 2 == 0) ? 'chetno' : 'nechetno';
+echo $result;
+
+echo '<br />';
+$x=10;
+$y=7;
+
+echo "$x + $y = " . ($x + $y);
+echo '<br />';
+echo "$x - $y = " . ($x - $y);
+echo '<br />';
+echo "$x * $y = " . ($x * $y);
+echo '<br />';
+echo "$x / $y = " . ($x / $y);
+echo '<br />';
+echo "$x % $y = " . ($x % $y);
+echo '<br />';
+
+
+
+
+
+
